0004f76e0a9404582787bd0957836a18b24db65e
Gerhard <gerhard.heide@web.de>
2021-09-05 18:26:47 +0200

    light

--- keyboards/bm40hsrgb/keymaps/noautos/keymap.c
+++ keyboards/bm40hsrgb/keymaps/noautos/keymap.c
@@ -21,6 +21,10 @@ void rgb_matrix_indicators_user(void) {
 switch (biton32(layer_state)) {
 case 0:
 rgb_matrix_set_color_all(0,1,10);
+rgb_matrix_set_color(34, 100, 10, 10); // cursor keys
+rgb_matrix_set_color(44, 100, 10, 10);
+rgb_matrix_set_color(45, 100, 10, 10);
+rgb_matrix_set_color(46, 100, 10, 10);
 break;
 case 1:
 //rgb_matrix_set_color_all(0,150,55); // all bright
@@ -35,8 +39,14 @@ rgb_matrix_set_color(25, 150, 150, 50);
 rgb_matrix_set_color(26, 150, 150, 50);
 rgb_matrix_set_color(27, 150, 150, 50);
 rgb_matrix_set_color(38, 150, 150, 50);
-rgb_matrix_set_color(7, 150, 150, 50); // ue oe
-rgb_matrix_set_color(9, 150, 150, 50); // oe
+rgb_matrix_set_color(7, 1, 150, 50); // ue
+rgb_matrix_set_color(9, 1, 150, 50); // oe
+rgb_matrix_set_color(21, 1, 150, 50); // 1
+rgb_matrix_set_color(33, 1, 150, 50); // 1
+rgb_matrix_set_color(34, 100, 10, 10); // cursor keys
+rgb_matrix_set_color(44, 100, 10, 10);
+rgb_matrix_set_color(45, 100, 10, 10);
+rgb_matrix_set_color(46, 100, 10, 10);
 break;
 case 2:
 rgb_matrix_set_color_all(0,1,60);
@@ -47,23 +57,24 @@ rgb_matrix_set_color(19, 150, 150, 50); // nav
 rgb_matrix_set_color(20, 150, 150, 50); // nav
 rgb_matrix_set_color(21, 150, 150, 50); // nav
 rgb_matrix_set_color(30, 150, 150, 50); // esc
+rgb_matrix_set_color(34, 100, 10, 10); // cursor keys
+rgb_matrix_set_color(44, 100, 10, 10);
+rgb_matrix_set_color(45, 100, 10, 10);
+rgb_matrix_set_color(46, 100, 10, 10);
 break;
 case 3:
 rgb_matrix_set_color_all(0,1,30);
-rgb_matrix_set_color(0, 150,100,10); 
-rgb_matrix_set_color(12, 150,100,10); 
-rgb_matrix_set_color(13, 150,100,10); 
-rgb_matrix_set_color(22, 150,100,10); 
-rgb_matrix_set_color(23, 150,100,10); 
-rgb_matrix_set_color(24, 150,100,10); 
-rgb_matrix_set_color(32, 150,100,10); 
-rgb_matrix_set_color(33, 150,100,10); 
-rgb_matrix_set_color(34, 150,100,10); 
-rgb_matrix_set_color(35, 150,100,10); 
-rgb_matrix_set_color(36, 150,100,10); 
-rgb_matrix_set_color(42, 150,100,10); 
-rgb_matrix_set_color(43, 150,100,10); 
-rgb_matrix_set_color(44, 150,100,10); 
+rgb_matrix_set_color(1, 150, 150, 50); // reset
+rgb_matrix_set_color(8, 150, 150, 50); // I I I
+rgb_matrix_set_color(20, 150, 150, 50);
+rgb_matrix_set_color(32, 150, 150, 50);
+rgb_matrix_set_color(6, 150, 150, 50);
+rgb_matrix_set_color(18, 150, 150, 50);
+rgb_matrix_set_color(30, 150, 150, 50);
+rgb_matrix_set_color(4, 150, 150, 50);
+rgb_matrix_set_color(16, 150, 150, 50);
+rgb_matrix_set_color(28, 150, 150, 50);
+
 break;
 case 4:
 rgb_matrix_set_color_all(0,1,30); 
@@ -77,14 +88,14 @@ rgb_matrix_set_color(25, 150, 150, 50);
 rgb_matrix_set_color(26, 150, 150, 50);
 rgb_matrix_set_color(27, 150, 150, 50);
 rgb_matrix_set_color(38, 150, 150, 50);
-rgb_matrix_set_color(17, 150, 150, 50); // makro
-rgb_matrix_set_color(18, 150, 150, 50);
-rgb_matrix_set_color(19, 150, 150, 50);
+rgb_matrix_set_color(19, 150, 150, 50); // makro
+rgb_matrix_set_color(20, 150, 150, 50);
+rgb_matrix_set_color(21, 150, 150, 50);
 rgb_matrix_set_color(7, 150, 150, 50);
 rgb_matrix_set_color(9, 150, 150, 50);
-rgb_matrix_set_color(53, 150, 150, 50);
-rgb_matrix_set_color(11, 150, 150, 50); // volume
-rgb_matrix_set_color(21, 150, 150, 50);
+rgb_matrix_set_color(33, 150, 150, 50);
+rgb_matrix_set_color(45, 50, 10, 10); // volume
+rgb_matrix_set_color(34, 50, 10, 10);
 break;
 }
 }
@@ -161,7 +172,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 */
-        
     }
     return true;
 };
