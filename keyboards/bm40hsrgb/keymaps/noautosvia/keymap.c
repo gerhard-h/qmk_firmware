af1509698da17e9d33d249e348ed68f3c1fd7314
Gerhard <gerhard.heide@web.de>
2021-10-03 21:14:39 +0200

    status list

--- keyboards/bm40hsrgb/keymaps/noautosvia/keymap.c
+++ keyboards/bm40hsrgb/keymaps/noautosvia/keymap.c
@@ -850,16 +850,15 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 ==============================================
  52(50) keys pysical keys 47
 
-* ` backtick is only available as Shift(tick) on L2
-* # is only available on L1
+* ` backtick is only available as Shift(tick) on L2  or ahk ,y
+* # is only available on L1 or ahk ,k
 * 
 now 9 x custom single_hold solution rjpsdfgtb
 some numbers 12347891 could also get a custom solution with different modifier handling 
 äüö behave similar to these numbers
 *
-* u h have will do/loose the same on dbltap and hold,  Auufer Buchhalter nachher 
 *
-* shift + backspace > delete not working (sends SFT(KC_DEL))
+* (bug) shift + backspace > delete not working (sends SFT(KC_DEL))
 *
 * h and u will now work on dbltap in Auufer, Buchhalter, nachher,... 
 *
@@ -873,59 +872,62 @@ some numbers 12347891 could also get a custom solution with different modifier h
 * DBL_TAP_HOLD m has autorepeat **** but DBL_TAP_HOLD * has no autorepeat
 * DBL_TAP_HOLD bsp has autorepeat
 * shift + SINGLE_HOLD n > (,
-* shift + KOMMA > > instead of < modifies the wrong key 
+* shift + DBL_TAP KOMMA > gives S(<) = > instead of < (maybe correct)
 * BUT shift + SINGLE_HOLD i > I, 
 * shift + SINGLE_HOLD q > Q instead of S(END)
 * Ä Ü Ö  works only shift neeeds to be passed through
-* shift + SINGLE_HOLD . > correct >
-* shift + SINGLE_HOLD - > correct /
-* alt + hold_4  works
+* shift + SINGLE_HOLD . > : correct DBL_TAP_HOLD > correct
+* shift + SINGLE_HOLD - > _ correct DBL_TAP_HOLD / correct
 * F10-12 only on L4 not L1 hold r = F10 instead of DELETE/€, F11 instead of 0  is sometimes confusing
 *
 * ESC/Home BSP/End needs all mods passed through 
 *
 * _each and _reset funtions are often the same and therefore shared between declarations
 * 
-* dbl tap esc does not sends ^ not even as dead key instead of custom key CIRCUMFL dance_dbltap_ could check "is keycode element of enum custom_keycodes?" than call that code
+* bug dbl_tap esc does not sends ^ not even as dead key instead of custom key CIRCUMFL dance_dbltap_ could check "is keycode element of enum custom_keycodes?" than call that code
 *
 * light_control if OSM(modifier) is locked
 * light intensity controls are inactive -> search solution in oryx keymap code
 *
 * shift + non shiftable key (e.g. A(C(KC_E))) outputs shift+€=nothing instead of ignoring the shift, but ignoring the shift in general does not work either
 * 
-* (bug) dance_mod_finished mods must be pressed one after the other > workaround swap + and | because + is a not shifted key wich could use MT
+* (bug) dance_mod_finished mods must be pressed one after the other 
+* else first key registers a tap instead of hold
+* > workaround swap + and | because + is a not shifted key wich could use MT
 *
-* (bug) modifier_dbldance_finished when interupted outputs the modifier instead of key. workaround: use MT 
+* (bug) modifier_dbldance_finished when interupted outputs the modifier instead of key (only with ALT). workaround: use MT 
 *
 * bug tap dance inside OSL only works if OSL key is held down | process_record_user  all OSM set status-flag on down and clear status-flag on up 
 * if tap_dance_each senses !status-flag & OSL active: OSL clear, permanent layer move...
 * ...tap dance continues ... on tapdance reset layer move 0
 * 
-* combos - are there use cases? do combos conflict with tap dance
-* c lower > enter
+* COMBOS - are there use cases? do combos conflict with tap dance ... yes they do but autohotkey can be used
+* combo starters: ,<alpha> q<alpha_minus_ulm> jf cv vc vz vd jz wt wd wf wz tricky: .<alpha> uu ii uv 
+* c + L1 > enter
 * qw = esc better than tapdance qq? ,k > # 
 * comma - combos ,q > 1 ,w > 2 ... ,. >  ,- > 
-* ,esc > F1 ,q > F2 ,bsp > F12
+* ,esc > F1 ,q > F2 ,bsp > F12 
 * comma diacrit opposit ,a > ä ,s > ß ,d > ö ,f > ü
-* D = LT(1,KC_D)  
+* G = LT(1,KC_G)  H = LT(1,KC_H) 
 * dot diacrit ., > ä .n > ü .l > ö .- > ß/alt
 * dot mix ., > ü .- > ö. ,. .. ä ,- > ß
 
-* home row mods? F, N ~ SFT
-*
+* (re)evaluate home row mods? F, N ~ SFT ... D, T ~ CTL ... G, H ~ L2/L1
+* 
+* reevaluate u h have will have no dbltap enabling:  Auufer Buchhalter nachher
 * reevaluate copy paste undo: xx cc vv zz
-* reevaluate alt-tab: kk vs L2z stick to kk
+* -reevaluate alt-tab: kk vs L2z ->. removed kk in favor of MT()
 * reevaluate alt-shift-tab: L2p
 * reevaluate 1=v on numpad1
-* reevaluate del key positioin RSFT L2DOT L1R LALT
-* reevaluate q END
+* reevaluate del key positioin RSFT L2DOT L1R LALT ,r
+* reevaluate q > END qq > @
 *
-* feature: single key alt tab {KC_LCTL,KC_LALT,KC_TAB}
+* feature idea: single key alt tab {KC_LCTL,KC_LALT,KC_TAB}
 * single hold & modus==OFF: alt + ctrl + tab & modus=ON 
 * tap & modus==ON: tab
 * single hold & modus==ON: modus==ON & Space  
 *       
-* Alt+F4: Ctl+(W+W) or (C+W)+W or C+(W+Q) or wq
+* Alt+F4: Ctl+(W+W) or (C+W)+W or C+(W+Q) or wq currenly L4 + K + A
 *
 *numberpad 2.0
 * €456-
